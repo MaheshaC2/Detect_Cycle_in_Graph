e979e373286537557aa1cb8d010d5e1a38e13412
MaheshaC2 <maheshac6475@gmail.com>
2026-04-13 22:52:52 +0530

    Eliminate unnecessary blank line in cycle.py

---
+++ cycle.py
@@ -0,0 +1,28 @@
+#Detect Cycle in Graph (DFS)
+
+graph = {
+    1: [2],
+    2: [3],
+    3: [1]
+}
+
+visited = set()
+rec_stack = set()
+
+def dfs(node):
+    if node in rec_stack:
+        return True
+    if node in visited:
+        return False
+
+    visited.add(node)
+    rec_stack.add(node)
+
+    for neighbor in graph[node]:
+        if dfs(neighbor):
+            return True
+
+    rec_stack.remove(node)
+    return False
+
+print(dfs(1))
